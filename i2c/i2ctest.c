b38e1359a74a2161556d1c701bccd0fb6789138b
ismailkoch492 <38835343+ismailkoch492@users.noreply.github.com>
2020-04-29 23:50:08 +0300

    Update i2ctest.c

--- i2c/i2ctest.c
+++ i2c/i2ctest.c
@@ -4,10 +4,17 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+void sendStart(void);
+
 int  main()
 {
   while(1)
   {
-
+    
   }
 }
+
+void sendStart()
+{
+  TWCR = (1<<TWINT)|(1<<TWSTA)| (1<<TWEN)
+]
