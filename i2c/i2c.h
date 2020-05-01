c5080368d2826e440566d01632a31fcde47200fd
ismailkoch492 <38835343+ismailkoch492@users.noreply.github.com>
2020-05-01 22:31:30 +0300

    Update i2c.h

--- i2c/i2c.h
+++ i2c/i2c.h
@@ -4,15 +4,21 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
+// Master Transmitter ---> S|SLA+W|a|DATA|a|P
+
+// Master Receiver ---> S|SLA+R|a|data|A|data|~A|P
+
 void sendStart(void);
 void startTransmitted(void);
 void checkTWSR_Start(void);
 
-void SLA_Wtransmitted_ACK_NACKreceived(void);
-void checkTWSR_MT_SLA_ACK(void);
+void SLA_W_ACK_NACKreceived(void);
+void checkMT_SLA_ACK(void);
 
 
-
+void sendDataTWDR(void);
+void dataACK_NACKreceived(void);
+void checkTWST_MT_DATA_ACK(void);
 void stopTransmitted(void);
 
 void sendStart(void)
@@ -31,12 +37,12 @@ void checkTWSR_Start(void)
     ERROR();
 }
   
-void SLA_Wtransmitted_ACK_NACKreceived(void);
+void SLA_W_ACK_NACKreceived(void);
 {
   while(!(TWCR & (1<<TWINT)));
 }
   
-void checkTWSR_MT_SLA_ACK(void)
+void checkMT_SLA_ACK(void)
 {
   if ((TWSR & 0xF8) != MT_SLA_ACK)
     ERROR();
