a9723d6416709671fd5d7f443b0f0449f86c2103
ismailkoch492 <38835343+ismailkoch492@users.noreply.github.com>
2020-05-01 22:51:18 +0300

    Update i2c.h

--- i2c/i2c.h
+++ i2c/i2c.h
@@ -23,49 +23,57 @@ void dataACK_NACKreceived(void);
 void checkMT_DATA_ACK(void);
 void stopTransmitted(void);
 
-void sendStart(void)
+void sendStart(void)  //1
 {
   TWCR = (1<<TWINT)|(1<<TWSTA)| (1<<TWEN);
 }
 
-void startTransmitted(void)
+void startTransmitted(void) //2
 {
   while (!(TWCR & (1<<TWINT)));
 }
   
-void checkStart(void)
+void checkStart(void) //3
 {
   if((TWSR & 0xF8) != START)
     ERROR();
 }
 
-void sendSLA_W(void)
+void sendSLA_W(void)  //4
 {
-  
+  TWDR = SLA_W;
+  TWCR = (1<<TWINT) | (1<<TWEN);
 }
   
-void SLA_W_ACK_NACKreceived(void)
+void SLA_W_ACK_NACKreceived(void) //5
 {
   while(!(TWCR & (1<<TWINT)));
 }
 
-void sendDAta(void)
-{
-  
-}
-
-void dataACK_NACKreceived(void)
-{
-  
-}
-  
-void checkMT_SLA_ACK(void)
+void checkMT_SLA_ACK(void) //6
 {
   if ((TWSR & 0xF8) != MT_SLA_ACK)
     ERROR();
 }
 
-void stopTransmitted(void)
+void sendDAta(void)
+{
+  TWDR = DATA;
+  TWCR = (1<<TWINT) | (1<<TWEN);
+}
+
+void dataACK_NACKreceived(void)
+{
+  while (!(TWCR & (1<<TWINT)));
+}
+
+void checkMT_DATA_ACK(void) //9
+{
+  if ((TWSR & 0xF8) != MT_DATA_ACK)
+    ERROR();
+}
+
+void stopTransmitted(void)  //10
 {
   TWCR = (1<<TWINT)|(1<<TWEN)| (1<<TWSTO);
 }
