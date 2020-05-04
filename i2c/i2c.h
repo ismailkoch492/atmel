76ffe436d936a690b056264d7c71685aa3670424
ismailkoch492 <38835343+ismailkoch492@users.noreply.github.com>
2020-05-04 18:43:58 +0300

    Update i2c.h

--- i2c/i2c.h
+++ i2c/i2c.h
@@ -5,47 +5,45 @@
 #include <util/delay.h>
 
 // Single Byte Write Sequence (MTSR)
-// Master Transmitter      S|SLA+W|-|DATA|-|DATA|-|P or S               sendStart();|sendSLA_W();|SLA_W_ACK_NACKreceived();|sendData();|dataACK_NACKreceived();|stopTransmitted(); 
-// Slave Receiver          -|-----|a|----|a|----|a|------               receiveStart();|receiveSLA_W();|3|4|5|6|7|8|9|stopReceived();|
+// Master Transmitter      S|SLA+W|-|DATA|-|DATA|-|P or S               sendStart();   |sendSLA_W();   |receiveSLA_W_ACK_NACK();|sendData();   |receiveDataACK_NACK();|sendData();|receiveDataACK_NACK();|sendStop();
+// Slave Receiver          -|-----|a|----|a|----|a|------               receiveStart();|receiveSLA_W();|sendSLA_W_ACK_NACK();   |receiveData();|sendDataACK_NACK();|receiveData();|sendDataACK_NACK();   |receiveStop();
 
 // Burst Write Sequence (MTSR)
-// Master Transmitter      S|SLA+W|-|DATA|-|DATA|-|DATA|-|P or S        sendStart();|sendSLA_W();|SLA_W_ACK_NACKreceived();|sendData();|dataACK_NACKreceived();|stopTransmitted(); 
-// Slave Receiver          -|-----|a|----|a|----|a|----|a|------        receiveStart();|receiveSLA_W();|3|4|5|6|7|8|9|stopReceived();|
+// Master Transmitter      S|SLA+W|-|DATA|-|DATA|-|DATA|-|P or S        sendStart();   |sendSLA_W();   |receiveSLA_W_ACK_NACK();|sendData();   |receiveDataACK_NACK();|sendData();   |receiveDataACK_NACK();|sendData();   |receiveDataACK_NACK();|sendStop();
+// Slave Receiver          -|-----|a|----|a|----|a|----|a|------        receiveStart();|receiveSLA_W();|sendSLA_W_ACK_NACK();   |receiveData();|sendDataACK_NACK();   |receiveData();|sendDataACK_NACK();   |receiveData();|sendDataACK_NACK();   |receiveStop();
 
 // Single Byte Read Sequence (MRST)
-// Master Transmitter      S|SLA+W|-|DATA|-|S|SLA+R|-|----|~A|P  
-// Slave Receiver          -|-----|a|----|a|-|-----|a|data|--|-  
+// Master Transmitter      S|SLA+W|-|DATA|-|S|SLA+R|-|----|~A|P         sendStart();   |sendSLA_W();   |receiveSLA_W_ACK_NACK();|sendData();   |receiveDataACK_NACK();|sendStart();   |sendSLA_R();   |receiveSLA_R_ACK_NACK();|receiveData();|sendDataNACK();       |sendStop();
+// Slave Receiver          -|-----|a|----|a|-|-----|a|data|--|-         receiveStart();|receiveSLA_W();|sendSLA_W_ACK_NACK();   |receiveData();|sendDataACK_NACK();   |receiveStart();|receiveSLA_R();|sendSLA_R_ACK_NACK();   |sendData();   |receiveDataACK_NACK();|receiveStop();
 
 // Burst Read Sequence (MRST)
-// Master Transmitter      S|SLA+W|-|DATA|-|S|SLA+R|-|----|A|----|~A|P  
-// Slave Receiver          -|-----|a|----|a|-|-----|a|data|-|data|--|-  
-
-// 
-// Master Receiver    ---> S|SLA+R|-|----|A|----|~A|P or S  ---> sendStart();|sendSLA_R();|SLA_R_ACK_NACKreceived();|receiveData();|dataACK_NACKreceived();|receiveData();|void dataACK_NACKsend(void);|stopTransmitted();
-// Slave Transmitter  ---> -|-----|a|data|-|data|--|------  ---> receiveStart();|receiveSLA_R();|3|4|5|6|7|8|9|stopReceived();|
-
+// Master Transmitter      S|SLA+W|-|DATA|-|S|SLA+R|-|----|A|----|~A|P  sendStart();   |sendSLA_W();   |receiveSLA_W_ACK_NACK();|sendData();   |receiveDataACK_NACK();|sendStart();   |sendSLA_R();   |receiveSLA_R_ACK_NACK();|receiveData();|sendDataACK_NACK();   |receiveData();|sendDataNACK();       |sendStop();
+// Slave Receiver          -|-----|a|----|a|-|-----|a|data|-|data|--|-  receiveStart();|receiveSLA_W();|sendSLA_W_ACK_NACK();   |receiveData();|sendDataACK_NACK();   |receiveStart();|receiveSLA_R();|sendSLA_R_ACK_NACK();   |sendData();   |receiveDataACK_NACK();|sendData();   |receiveDataACK_NACK();|receiveStop();
 
 
 void sendStart(void);//1
-void receiveStart(void//1
+void receiveStart(void)//1
 void startTransmitted(void);//2
 void checkStart(void);//3
 void sendSLA_W(void);//4
 void sendSLA_R(void);//4
 void receiveSLA_W(void);//4
 void receiveSLA_R(void);//4
-void SLA_W_ACK_NACKreceived(void);//5
-void SLA_R_ACK_NACKreceived(void);//5
+void sendSLA_W_ACK_NACK(void);
+void sendSLA_R_ACK_NACK(void);
+void receiveSLA_W_ACK_NACK(void);//5
+void receiveSLA_R_ACK_NACK(void);//5
 void checkMT_SLA_ACK(void);//6
 void checkMR_SLA_ACK(void);//6
 void sendData(int DATA);//7
 void receiveData(int DATA);//7
-void dataACK_NACKreceived(void);//8
-void dataACK_NACKsend(void);//8
+void sendDataACK_NACK(void);//8
+void sendDataNACK(void);//8
+void receiveDataACK_NACK(void);//8
 void checkMT_DATA_ACK(void);//9
 void checkMR_DATA_ACK(void);//9
-void stopTransmitted(void);//10
-void stopReceived(void);//10
+void sendStop(void);//10
+void receiveStop(void);//10
 
 void sendStart(void)  //1
 {
@@ -56,7 +54,7 @@ void startTransmitted(void) //2
 {
   while (!(TWCR & (1<<TWINT)));
 }
-  
+
 void checkStart(void) //3
 {
   if((TWSR & 0xF8) != START)
@@ -74,13 +72,13 @@ void sendSLA_R(void)  //4
   TWDR = SLA_R;
   TWCR = (1<<TWINT) | (1<<TWEN);
 }
-  
-void SLA_W_ACK_NACKreceived(void) //5
+
+void receiveSLA_W_ACK_NACK(void) //5
 {
   while(!(TWCR & (1<<TWINT)));
 }
 
-void SLA_R_ACK_NACKreceived //5
+void receiveSLA_R_ACK_NACK //5
 {
   while(!(TWCR & (1<<TWINT)));
 }
@@ -105,17 +103,17 @@ void sendData(int DATA) //7
 
 void receiveData(void) //7
 {
-  
+
 }
 
-void dataACK_NACKreceived(void) //8
+void receiveDataACK_NACK(void) //8
 {
   while (!(TWCR & (1<<TWINT)));
 }
 
-void dataACK_NACKsend(void) //
+void sendDataACK_NACK(void) //
 {
-  
+
 }
 
 void checkMT_DATA_ACK(void) //9
@@ -126,10 +124,10 @@ void checkMT_DATA_ACK(void) //9
 
 void checkMR_DATA_ACK(void) //9
 {
-  
+
 }
 
-void stopTransmitted(void)  //10
+void sendStop(void)  //10
 {
   TWCR = (1<<TWINT)|(1<<TWEN)| (1<<TWSTO);
 }
