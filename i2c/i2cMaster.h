5d914565a218c56a78bdccc957de5d5ee057323e
İsmail KOÇ <ismailkoc@Ismail-MacBook-Pro.local>
2020-05-08 23:03:13 +0300

    Repeated start function added.

--- i2c/i2cMaster.h
+++ i2c/i2cMaster.h
@@ -22,12 +22,12 @@
 // Slave Receiver          -|-----|a|----|a|----|a|----|a|------        receiveStart();|receiveSLA_W();|sendSLA_W_ACK_NACK();   |receiveData();|sendDataACK_NACK();   |receiveData();|sendDataACK_NACK();   |receiveData();|sendDataACK_NACK();   |receiveStop();
 
 // Single Byte Read Sequence (MRST)
-// Master Transmitter      S|SLA+W|-|DATA|-|S|SLA+R|-|----|~A|P         sendStart();   |sendSLA_W();   |receiveSLA_W_ACK_NACK();|sendData();   |receiveDataACK_NACK();|sendStart();   |sendSLA_R();   |receiveSLA_R_ACK_NACK();|receiveData();|sendDataNACK();       |sendStop();
-// Slave Receiver          -|-----|a|----|a|-|-----|a|data|--|-         receiveStart();|receiveSLA_W();|sendSLA_W_ACK_NACK();   |receiveData();|sendDataACK_NACK();   |receiveStart();|receiveSLA_R();|sendSLA_R_ACK_NACK();   |sendData();   |receiveDataACK_NACK();|receiveStop();
+// Master Transmitter      S|SLA+W|-|DATA|-|S|SLA+R|-|----|~A|P         sendStart();   |sendSLA_W();   |receiveSLA_W_ACK_NACK();|sendRepeatedData();|receiveDataACK_NACK();|sendStart();   |sendSLA_R();   |receiveSLA_R_ACK_NACK();|receiveData();|sendDataNACK();       |sendStop();
+// Slave Receiver          -|-----|a|----|a|-|-----|a|data|--|-         receiveStart();|receiveSLA_W();|sendSLA_W_ACK_NACK();   |receiveData();     |sendDataACK_NACK();   |receiveStart();|receiveSLA_R();|sendSLA_R_ACK_NACK();   |sendData();   |receiveDataACK_NACK();|receiveStop();
 
 // Burst Read Sequence (MRST)
-// Master Transmitter      S|SLA+W|-|DATA|-|S|SLA+R|-|----|A|----|~A|P  sendStart();   |sendSLA_W();   |receiveSLA_W_ACK_NACK();|sendData();   |receiveDataACK_NACK();|sendStart();   |sendSLA_R();   |receiveSLA_R_ACK_NACK();|receiveData();|sendDataACK_NACK();   |receiveData();|sendDataNACK();       |sendStop();
-// Slave Receiver          -|-----|a|----|a|-|-----|a|data|-|data|--|-  receiveStart();|receiveSLA_W();|sendSLA_W_ACK_NACK();   |receiveData();|sendDataACK_NACK();   |receiveStart();|receiveSLA_R();|sendSLA_R_ACK_NACK();   |sendData();   |receiveDataACK_NACK();|sendData();   |receiveDataACK_NACK();|receiveStop();
+// Master Transmitter      S|SLA+W|-|DATA|-|S|SLA+R|-|----|A|----|~A|P  sendStart();   |sendSLA_W();   |receiveSLA_W_ACK_NACK();|sendData();   |receiveDataACK_NACK();|sendRepeatedStart();|sendSLA_R();   |receiveSLA_R_ACK_NACK();|receiveData();|sendDataACK_NACK();   |receiveData();|sendDataNACK();       |sendStop();
+// Slave Receiver          -|-----|a|----|a|-|-----|a|data|-|data|--|-  receiveStart();|receiveSLA_W();|sendSLA_W_ACK_NACK();   |receiveData();|sendDataACK_NACK();   |receiveStart();     |receiveSLA_R();|sendSLA_R_ACK_NACK();   |sendData();   |receiveDataACK_NACK();|sendData();   |receiveDataACK_NACK();|receiveStop();
 
 #if I2C_PRES == 1
 TWSR &= ~(1<<TWSP1)|(1<<TWSP0);
@@ -44,9 +44,11 @@ TWSR |= (1<<TWSP1)|(1<<TWSP0);
 TWBR = (F_CPU/F_SCL - 16)/(2*I2C_PRES);
 
 void sendStart(void);//1
+void sendRepeatedStart(void);//1
 void receiveStart(void);//1
 void startTransmitted(void);//2
 void checkStart(void);//3
+void checkRepeatedStart(void);//3
 void sendSLA_W(int SLA_W);//4
 void sendSLA_R(int SLA_R);//4
 void receiveSLA_W(void);//4
@@ -78,6 +80,13 @@ void sendStart(void)  //1
   checkStart(); //3
 }
 
+void sendRepeatedStart(void)
+{
+  TWCR = (1<<TWINT)|(1<<TWSTA)| (1<<TWEN);
+  startTransmitted(); //2
+  checkRepeatedStart(); //3
+}
+
 void startTransmitted(void) //2
 {
   while (!(TWCR & (1<<TWINT)));
@@ -86,6 +95,12 @@ void startTransmitted(void) //2
 void checkStart(void) //3
 {
   if((TWSR & 0xF8) != TW_START)
+    ERROR();
+}
+
+void checkRepeatedStart(void)
+{
+  if((TWSR & 0xF8) != TW_REP_START)
     ERROR();
 }
 
@@ -201,7 +216,7 @@ void SingleMRST(int slaveAddress, int registerAddress, int data)
   receiveSLA_W_ACK_NACK();
   sendData(registerAddress);
   receiveDataACK_NACK();
-  sendStart();
+  sendRepeatedStart();
   sendSLA_R();
   receiveSLA_R_ACK_NACK();
   receiveData(data);
@@ -216,7 +231,7 @@ void BurstMRST(int slaveAddress, int registerAddress, int firstData, int secondD
   receiveSLA_W_ACK_NACK();
   sendData(registerAddress);
   receiveDataACK_NACK();
-  sendStart();
+  sendRepeatedStart();
   sendSLA_R();
   receiveSLA_R_ACK_NACK();
   receiveData(firstData);
