d30310720174965148daf5bb56b49d8f88a8bf53
İsmail KOÇ <ismailkoc@Ismail-MacBook-Pro.local>
2020-05-08 22:29:21 +0300

    Slave addresses, master functions added.

--- i2c/i2ctest.c
+++ i2c/i2ctest.c
@@ -1,8 +1,12 @@
-#define F_CPU 16000000
+#define F_CPU 16000000UL
+#define SLA1 0x68 //MPU9250
+#define SLA2 0x77 //BMP180-BMP280
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <util/delay.h>
+#include <util/twi.h>
+#include "i2cMaster.h"
 
 int main()
 {
