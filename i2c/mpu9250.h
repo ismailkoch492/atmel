f3f40dba2ca0242c58eb63b0375084cd8b957c97
İsmail KOÇ <ismailkoc@Ismail-MacBook-Pro.local>
2020-05-10 23:36:10 +0300

    i2cMaster.h included and sensor data functions added.

--- i2c/mpu9250.h
+++ i2c/mpu9250.h
@@ -6,6 +6,7 @@
 #include <util/delay.h>
 #include <util/twi.h>
 #include <stdlib.h>
+#include <i2cMaster.h>
 
 #define ACCEL_XOUT_H  0x3B
 #define ACCEL_XOUT_L  0x3C
@@ -28,4 +29,52 @@
 #define HYH 0x06
 #define HZL 0x07
 #define HZH 0x08
+
+int getAccelData(void);
+int getGyroData(void);
+int getTempData(void);
+int getMagData(void);
+
+int getAccelData(void)
+{
+  int accelData[3];
+
+  accelData[0] = ((SingleMRST(SLA1, ACCEL_XOUT_H) << 8) | SingleMRST(SLA1, ACCEL_XOUT_L));
+  accelData[1] = ((SingleMRST(SLA1, ACCEL_YOUT_H) << 8) | SingleMRST(SLA1, ACCEL_YOUT_L));
+  accelData[2] = ((SingleMRST(SLA1, ACCEL_ZOUT_H) << 8) | SingleMRST(SLA1, ACCEL_ZOUT_L));
+
+  return(accelData);
+}
+
+int getGyroData(void)
+{
+  int gyroData[3];
+
+  gyroData[0] = ((SingleMRST(SLA1, GYRO_XOUT_H) << 8) | SingleMRST(SLA1, GYRO_XOUT_L));
+  gyroData[1] = ((SingleMRST(SLA1, GYRO_YOUT_H) << 8) | SingleMRST(SLA1, GYRO_YOUT_L));
+  gyroData[2] = ((SingleMRST(SLA1, GYRO_ZOUT_H) << 8) | SingleMRST(SLA1, GYRO_ZOUT_L));
+
+  return(gyroData);
+}
+
+int getTempData(void)
+{
+  int tempData;
+
+  tempData = ((SingleMRST(SLA1, TEMP_OUT_H) << 8) | SingleMRST(SLA1, TEMP_OUT_L));
+
+  return(tempData);
+}
+
+int getMagData(void)
+{
+  int magData[3];
+
+  magData[0] = ((SingleMRST(SLA1, HXH) << 8) | SingleMRST(SLA1, HXL));
+  magData[1] = ((SingleMRST(SLA1, HYH) << 8) | SingleMRST(SLA1, HYL));
+  magData[2] = ((SingleMRST(SLA1, HZH) << 8) | SingleMRST(SLA1, HZL));
+
+  return(magData);
+}
+
 #endif
